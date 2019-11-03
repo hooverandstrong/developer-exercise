2a48ee94b915cd4467ae9636cf8a3fa4f8fb74f1
Joe Flynn <joflynn@gmail.com>
2019-11-03 10:14:32 -0500

    Set up basic layout

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Header() {
+
+  return (
+    <div className="main">
+      <aside />
+      <div className="center">
+      </div>
+      <aside />
+    </div>
+  );
+}
